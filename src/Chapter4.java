87c19989951d04cb2fe20f4d252f7702c2fa7c62
Yunhong Min <yunhongmin00@gmail.com>
2022-08-03 16:29:05 +0900

    Chapter4 switch

---
+++ src/Chapter4.java
@@ -0,0 +1,28 @@
+public class Chapter4 {
+    public static void main(String[] args) {
+        PrintSeason(5);
+        PrintSeason(7);
+        PrintSeason(10);
+        PrintSeason(1);
+        PrintSeason(13);
+    }
+
+    public static void PrintSeason(int month) {
+        switch (month) {
+            case 3: case 4: case 5:
+                System.out.println("Spring");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("Summer");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("Fall");
+                break;
+            case 12: case 1: case 2:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Wrong month");
+        }
+    }
+}
