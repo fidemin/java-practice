5ff10c481b854b6361d9fb110787526e3fefd8d8
Yunhong Min <yunhongmin00@gmail.com>
2022-08-05 16:45:57 +0900

    initialization block

--- src/Chapter06.java
+++ src/Chapter06.java
@@ -3,9 +3,12 @@ public class Chapter06 {
         System.out.println(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
         Car car1 = new Car("blue", "auto", 4);
-        Car car2 = new Car("red");
         System.out.println(car1);
+        System.out.println(Car.getNumOfCars());
+
+        Car car2 = new Car("red");
         System.out.println(car2);
+        System.out.println(Car.getNumOfCars());
     }
 
     static int add(int... nums) {
@@ -19,13 +22,16 @@ public class Chapter06 {
 }
 
 class Car {
+    // numOfCars initialized to 0: member var
     static int numOfCars;
     static {
-       numOfCars = 0;
+        System.out.println("numOfCars: " + numOfCars);
     }
 
     {
+        // called before constructor
         numOfCars++;
+        System.out.println("numOfCars: " + numOfCars);
     }
 
     String color;
@@ -40,6 +46,10 @@ class Car {
 
     Car(String color) {
         this(color, "auto", 4);
+    }
+
+    static int getNumOfCars() {
+        return numOfCars;
     }
 
     @Override
