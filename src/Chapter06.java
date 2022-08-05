68eb7f3c10cf237df973bd494a3e0082af919612
Yunhong Min <yunhongmin00@gmail.com>
2022-08-05 16:25:35 +0900

    constructor

--- src/Chapter06.java
+++ src/Chapter06.java
@@ -1,6 +1,9 @@
 public class Chapter06 {
     public static void main(String[] args) {
         System.out.println(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        Car car = new Car("blue", "auto", 4);
+        System.out.println(car);
     }
 
     static int add(int... nums) {
@@ -9,5 +12,22 @@ public class Chapter06 {
             result += num;
         }
         return result;
+    }
+}
+
+class Car {
+    String color;
+    String gearType;
+    int numOfDoors;
+
+    Car(String color, String gearType, int numOfdoors) {
+        this.color = color;
+        this.gearType = gearType;
+        this.numOfDoors = numOfdoors;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("color=%s, gearType=%s, numOfDoors=%d", this.color, this.gearType, this.numOfDoors);
     }
 }
