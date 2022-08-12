6d5a8b8ec5934208abb3f481454e12e0ba2c2e08
Yunhong Min <yunhongmin00@gmail.com>
2022-08-12 10:48:25 +0900

    chapter08: throw exception

--- src/javabasic/Chapter08.java
+++ src/javabasic/Chapter08.java
@@ -13,11 +13,38 @@ public class Chapter08 {
             Exception e = new Exception("intended exception");
             throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        IntArithmetic a1 = new IntArithmetic();
+        try {
+            a1.divide(2, 0);
+        } catch (IntArithmeticException e) {
+            System.out.println(e.getMessage());
         }
 
         throw new RuntimeException(); // RuntimeException is not required to be dealt: unchecked exception
     }
+}
 
+class IntArithmeticException extends Exception {
+    public IntArithmeticException() {
+       super();
+    }
+    public IntArithmeticException(String message) {
+       super(message);
+    }
+}
 
+class IntArithmetic {
+    int divide(int a, int b) throws IntArithmeticException {
+
+        int result;
+        try {
+            result = a / b;
+        } catch (ArithmeticException e) {
+            throw new IntArithmeticException("divided by zero");
+        }
+        return result;
+    }
 }
