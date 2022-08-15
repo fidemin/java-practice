efc4316a3b41be076f7dfd8da2e61a5427e77abb
Yunhong Min <yunhongmin00@gmail.com>
2022-08-15 19:51:11 +0900

    LocalTime, DateTime...

--- src/javabasic/Chapter10.java
+++ src/javabasic/Chapter10.java
@@ -1,12 +1,14 @@
 package javabasic;
 
-import javax.security.auth.callback.CallbackHandler;
 import java.text.DecimalFormat;
 import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Chapter10 {
     final static String[] DAY_OF_WEEK = {"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
@@ -14,7 +16,6 @@ public class Chapter10 {
     public static void main(String[] args){
         Calendar cal1 = Calendar.getInstance();
         printCalendar(cal1);
-
 
         Calendar cal2 = Calendar.getInstance();
         cal2.set(Calendar.MONTH, Calendar.JULY);
@@ -34,7 +35,6 @@ public class Chapter10 {
         Date d = new Date(cal1.getTimeInMillis());
         System.out.println(sdf.format(d));
 
-
         SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
         try {
             Date date = sdf2.parse("2022-08-22");
@@ -47,10 +47,45 @@ public class Chapter10 {
         Object[] arguments = {"Yunhong", "22"};
         System.out.println(mf1.format(arguments));
 
+        // LocalDate and LocalTime are immutable
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        System.out.println(date.withYear(2021));
+        System.out.println(date.plusMonths(1));
+        System.out.println(time.truncatedTo(ChronoUnit.HOURS));
+
+        LocalDate birthDate = LocalDate.of(1984,2, 17);
+        LocalTime birthTime = LocalTime.of(23,2, 17);
+
+
+        Instant now = Instant.now();
+        System.out.println(now.toEpochMilli() + " " + now.getEpochSecond() + " " + now.getNano());
+
+        // Date to instance
+        Instant ist1 = d.toInstant();
+
+        // LocalDateTime, ZonedDateTime
+        LocalDateTime ldt = LocalDateTime.of(date, time);
+        System.out.println(ldt);
+
+        ZoneId zid = ZoneId.of("Asia/Seoul");
+        ZonedDateTime zdt = ldt.atZone(zid);
+        System.out.println(zdt);
+        OffsetDateTime odt = zdt.toOffsetDateTime();
+        System.out.println(odt);
+
+        GregorianCalendar gCal = GregorianCalendar.from(zdt);
+        printCalendar(gCal);
+
+        ZoneId zid2 = ZoneId.of("America/New_York");
+        System.out.println(ZonedDateTime.now().withZoneSameInstant(zid2));
+
+
+
     }
 
     static void printCalendar(Calendar cal) {
-        System.out.printf("%d-%02d-%02d %02d:%02d:%02d+%02d:00 %s\n",
+        System.out.printf("%d-%02d-%02dT%02d:%02d:%02d+%02d:00 %s\n",
                 cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH),
                 cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND),
                 cal.get(Calendar.ZONE_OFFSET) / (60 * 60 * 1000), DAY_OF_WEEK[cal.get(Calendar.DAY_OF_WEEK)]);
