ecad9af7709ff12a97f2c2f61bfdc52e3f568e16
Yunhong Min <yunhongmin00@gmail.com>
2022-08-21 15:13:25 +0900

    annotation

--- src/javabasic/Chapter12.java
+++ src/javabasic/Chapter12.java
@@ -1,5 +1,9 @@
 package javabasic;
 
+import sun.rmi.runtime.NewThreadAction;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 
 public class Chapter12 {
@@ -15,6 +19,16 @@ public class Chapter12 {
 
         System.out.println(Juicer.makeJuice(fruitBox));
         System.out.println(Juicer.makeJuice(appleBox));
+
+        NewClass nc = new NewClass();
+        nc.oldField = 10;
+        System.out.println(nc.getOldField());
+
+        @SuppressWarnings("unchecked")
+        ArrayList<NewClass> list = new ArrayList();
+        list.add(nc);
+
+        AnnotationEx.run();
     }
 }
 
@@ -66,3 +80,47 @@ class Juicer {
         return new Juice(b.toString());
     }
 }
+
+class NewClass {
+    int newField;
+
+    @Deprecated
+    int oldField;
+
+    int getNewField() {
+        return this.newField;
+    }
+
+    @Deprecated
+    int getOldField() {
+        return this.oldField;
+    }
+}
+
+@TestInfo(testedBy="Yunhong Min", testDate=@DateTime(yymmdd="20220101", hhmmss="111111"))
+class AnnotationEx {
+    static void run() {
+
+        Class<AnnotationEx> cls = AnnotationEx.class;
+        TestInfo anno = (TestInfo) cls.getAnnotation(TestInfo.class);
+        System.out.println(anno.testedBy());
+        System.out.println(anno.testDate().yymmdd());
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface TestInfo {
+    int count() default 1;
+    String testedBy();
+    TestType testType() default TestType.FIRST;
+    String[] testTools() default "JUnit";
+    DateTime testDate();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface DateTime {
+    String yymmdd();
+    String hhmmss();
+}
+
+enum TestType { FIRST, FINAL }
